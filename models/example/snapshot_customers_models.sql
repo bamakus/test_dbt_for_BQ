8077318a037ee200d3cd3ab6410f7ba076e2f369
Bamakus <37049053+bamakus@users.noreply.github.com>
2024-01-09 13:18:49 +0100

    Update snapshot_customers_models.sql

--- models/example/snapshot_customers_models.sql
+++ models/example/snapshot_customers_models.sql
@@ -1,8 +1,12 @@
--- product_snapshot_model.sql
+{% snapshot users_snapshot %}  
+--  In this strategy, dbt will create a new snapshot record whenever the timestamp column is more recent than the last time the snapshot was run
+-- for TEST  virtual timestamp value created each time a RUN 
 {{ config(
-    materialized='snapshot',
-    unique_key='cus.id, snapshot_timestamp'
-) }}
+   target_schema='snapshots',      
+    strategy='timestamp',      
+    unique_key='cus.id',      
+    updated_at='snapshot_timestamp'
+    ) }}
 
 
 SELECT
@@ -14,3 +18,5 @@ cus.first_name,
 d.*
  FROM `{{env_var('DBT_BQ_PROJECT')}}.{{env_var('DBT_BQ_DATASET')}}.customers` cus
 join unnest(addresses) as d	
+
+    {% endsnapshot %}
