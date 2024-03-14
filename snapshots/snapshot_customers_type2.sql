e0608c41a7b6608edfe391efdf3f7a69085dc326
Bamakus <37049053+bamakus@users.noreply.github.com>
2024-03-14 14:44:55 +0100

    maj

--- snapshots/snapshot_customers_type2.sql
+++ snapshots/snapshot_customers_type2.sql
@@ -13,10 +13,8 @@
 SELECT
 CURRENT_TIMESTAMP() AS snapshot_timestamp,    
 cus.id,
-cus.last_name,
-cus.dob,
 cus.first_name,
-d.*
+d.status
  FROM `{{env_var('DBT_BQ_PROJECT')}}.{{env_var('DBT_BQ_DATASET')}}.customers` cus
 join unnest(addresses) as d	
 {% endsnapshot %}
