6d4773108932f047dbc119e7aeec0e77bbe77651
Bamakus <37049053+bamakus@users.noreply.github.com>
2024-03-14 12:06:35 +0100

    Create customers_type1 -merge.sql

---
+++ models/example/customers_type1 -merge.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized='incremental',         
+        unique_key="id||'-'||status", 
+        strategy = "merge" 
+    )
+}}
+
+SELECT   
+cus.id,
+cus.dob,
+cus.last_name, 
+d.*
+ FROM `test-bi-query-291307.dbt_demo_bigquery.customers` cus
+join unnest(addresses) as d
+
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records arriving later on the same day as the last run of this model)
+  --where date_day >= (select max(date_day) from {{ this }})
+
+{% endif %}
+
+
+	
